a7c893679a45505088356c53a786a51cd30b5bd2
chelsey-mae <chelseypeace12345@gmail>
2025-03-06 03:31:03 -0500

    I edited the docstring in the function get_escape_time.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,6 +1,6 @@
 def get_escape_time(c:complex, max_iterations: int) -> int | None:
-    """ Return int - the number of iterations which pass before c escapes -
-      or None - if c does not escape (< 2) in the specified number of iterations """
+    """Returns int - the number of iterations which pass before c escapes -
+      or None - if c does not escape (< 2) in the specified number of iterations"""
     number_of_iterations = 0
     real = c.real
     imaginary = c.imag
