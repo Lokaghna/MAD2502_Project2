ee13d43bf7e17479f2c4c1ca3b92983e4edb1c36
Lokaghna <143106269+Lokaghna@users.noreply.github.com>
2025-03-12 11:26:32 -0400

    Update mandelbrot.py
    Visual cleaning

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,8 +4,10 @@ import numpy as np
 def get_escape_time(c: complex,
                     max_iterations: int
                     ) -> int | None:
-    """Returns int - the number of iterations which pass before c escapes -
-        or None - if c does not escape (< 2) in the specified number of iterations"""
+    """
+    Returns int - the number of iterations which pass before c escapes.
+    Returns None - if c does not escape (< 2) in the specified number of iterations
+    """
 
     z = 0
 
@@ -21,9 +23,11 @@ def get_complex_grid(top_left: complex,
                      bottom_right: complex,
                      step: float
                      ) -> np.ndarray:
-    """Compute a grid of complex numbers, one for each pixel of our image;
-        returns array - contents will be complex numbers evenly spaced between top_left
-        and (but not including) bottom_right"""
+    """
+    Compute a grid of complex numbers, one for each pixel of our image;
+    returns array - contents will be complex numbers evenly spaced between top_left
+    and (but not including) bottom_right
+    """
 
     real_num = np.arange(top_left.real, bottom_right.real, step)  # found the attributes - real and imag - using dir(np)
     imag_num = np.arange(top_left.imag, bottom_right.imag, -step)
@@ -37,13 +41,15 @@ def get_escape_time_color_arr(
         c_arr: np.ndarray,
         max_iterations: int
 ) -> np.ndarray:
-    """Compute the escape of the color array for a given grid of complex numbers based on an iteration process
+    """
+    Compute the escape of the color array for a given grid of complex numbers based on an iteration process
     Return:
     --------
     An array that has the same shape with the c_arr, containing values in the range[0,1] so that:
     -Points that never escape are colored black(0.0)
     -Points with 0 escape time are colored white(1.0)
-    -Points with maximum escape time are colored with 1/(max_iterations+1), which is close to 0.0"""
+    -Points with maximum escape time are colored with 1/(max_iterations+1), which is close to 0.0
+    """
 
     a = np.zeros_like(c_arr, dtype=np.complex128)
     escape_time = np.full(c_arr.shape, max_iterations + 1, dtype=int)
