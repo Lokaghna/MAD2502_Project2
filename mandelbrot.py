379cc909fc8e5fb7259fb55ae4fc797d31ab0c19
Lokaghna <lokaghna.velugub@ufl.edu>
2025-03-12 10:35:53 -0400

    Simplified logic for get_complex_grid function.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,33 +1,36 @@
 import numpy as np
 
 
-def get_escape_time(c:complex, max_iterations: int) -> int | None:
+def get_escape_time(c: complex,
+                    max_iterations: int
+                    ) -> int | None:
     """Returns int - the number of iterations which pass before c escapes -
         or None - if c does not escape (< 2) in the specified number of iterations"""
 
     z = 0
 
-    for num in range(max_iterations+1):
+    for num in range(max_iterations + 1):
         sequence = z = (z * z) + c
         if abs(sequence) > 2:
             return num
     return None
 
 
-def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
+def get_complex_grid(top_left: complex,
+                     bottom_right: complex,
+                     step: float
+                     ) -> np.ndarray:
     """Compute a grid of complex numbers, one for each pixel of our image;
         returns array - contents will be complex numbers evenly spaced between top_left
         and (but not including) bottom_right"""
 
-    real_num = np.arange(top_left.real, bottom_right.real, step) #found the attributes - real and imag - using dir(np)
+    real_num = np.arange(top_left.real, bottom_right.real, step)  # found the attributes - real and imag - using dir(np)
     imag_num = np.arange(top_left.imag, bottom_right.imag, -step)
 
-    reshape_real = np.reshape(real_num, (1, len(real_num)))
-    reshape_imag = np.reshape(imag_num, (1, len(imag_num), 1))
-
-    final_grid = reshape_real + (1j * reshape_imag)
+    final_grid = real_num[None, :] + 1j * imag_num[:, None]
 
     return final_grid
+
 
 def get_escape_time_color_arr(
         c_arr: np.ndarray,
@@ -40,6 +43,7 @@ def get_escape_time_color_arr(
     -Points that never escape are colored black(0.0)
     -Points with 0 escape time are colored white(1.0)
     -Points with maximum escape time are colored with 1/(max_iterations+1), which is close to 0.0"""
+
     a = np.zeros_like(c_arr)
     escape_time = np.full(c_arr.shape, max_iterations + 1)
     escape_points = np.ones(c_arr.shape, dtype=bool)
@@ -49,14 +53,15 @@ def get_escape_time_color_arr(
         escape_time[escaped & escape_points] = n
         escape_points[escaped] = False
 
-    color = (max_iterations - escape_time +1 )/ (max_iterations + 1)
+    color = (max_iterations - escape_time + 1) / (max_iterations + 1)
     color[escape_time == 0] = 1.0
     return color.reshape(color.shape[1:])
 
-def get_julia_color_arr(z_arr: np.ndarray,   # 2D grid of initial z-values (complex)
-                        c: complex,          # Fixed complex parameter for z^2 + c
+
+def get_julia_color_arr(z_arr: np.ndarray,  # 2D grid of initial z-values (complex)
+                        c: complex,  # Fixed complex parameter for z^2 + c
                         max_iterations: int
-) -> np.ndarray:
+                        ) -> np.ndarray:
     """
         Return a 2D array of grayscale values in [0,1], representing how quickly
         each point z in z_arr escapes under z_{n+1} = z_n^2 + c.
